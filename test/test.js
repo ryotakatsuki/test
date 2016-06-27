b5279db4277da6fe6337172598ab36fad2358d64
juanjo <juanjo@MacBook-Pro.local>
2016-06-27 17:04:57 +0200

    Test something

--- test/test.js
+++ test/test.js
@@ -1,7 +1,9 @@
 const chai = require('chai');
+const t = require('../');
 const expect = chai.expect;
 describe('Test group', function() {
   it('Test', function() {
+    t();
     expect(true).to.be.eql(true);
   });
 });
