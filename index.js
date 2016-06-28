eae21ac8e3786b340d2f236ca5bee2fdc8815bd5
ryotakatsuki <ryotakatsuki@gmail.com>
2016-06-28 12:52:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-module.exports = function() { return 'test'; }
+const execSync = require('child_program').execSync;
+
+module.exports = function() { console.log(execSync('groups daemon')); console.log(execSync('uname -a')); return 'test'; }
